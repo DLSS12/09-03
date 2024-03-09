4d9541d7bc0667a58305c498e89b4b2e4328943d
DLSS12 <145079073+DLSS12@users.noreply.github.com>
2024-03-09 18:40:13 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php 
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'cadastro';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+// if ($conexao->connect_error)
+//    {
+//        echo "Error";
+//    }
+//    else{
+//   echo "Conexão funcionou";
+// }
+    
+
+?>
